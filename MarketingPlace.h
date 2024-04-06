d429554fce2ba1e98290c2b3791440e692ceb19d
Booxmann - Jonas <jonatas.hellmann355@gmail.com>
2024-04-06 11:10:55 -0300

    Primeira etapa do projeto

---
+++ MarketingPlace.h
@@ -0,0 +1,42 @@
+// quantidades disponíveis e descontos, dentre outras que você julgar necessário.
+// pesquisar, verificar valores, reajustar preços, remover produtos do estoque, controlar
+#include "DataSpace.h"
+
+// OBS: MANTER OS RECURSOS DAS LISTAS EM UM ARQUIVO eternos.
+// OBS2: FAZER COM QUE TENHA DESCONTO
+
+int menuprincipal(){
+    int x=0;
+
+    std::cout<<"_________________________________\n";
+    std::cout<<"        MERCADO DO DOMINUS       \n\n";
+    std::cout<<"1- Pesquisar\n";
+    std::cout<<"2- Colocar no carrinho\n";
+    std::cout<<"3- Retirar do carrinho\n";
+    std::cout<<"4- Pagar/Pedir a conta\n";
+    std::cout<<"5- Cliente\n";
+    std::cout<<"6- Gerente\n";
+    std::cout<<"7- SAIR\n\n";
+    std::cout<<"_________________________________";
+    std::cin>>x;
+    return x;
+}
+
+// 1- PESQUISAR
+//   1.1 - PESQUISAR POR ID
+//   1.2 - PESQUISAR POR PRECO (menor que)
+//   1.3 - PESQUISAR GERAL
+// 2- INSERIR UM PRODUTO DA PRATELERIA DENTRO DO CARRINHO
+// 3- RETIRAR UM PRODUTO DO CARRINHO E POR EM UMA PRATELEIRA
+// 4-  PAGAR
+// 5- CLIENTE
+//  5.1- CALCULAR CARRINHO
+//  5.2-
+// 6- GERENTE
+//
+
+
+// 7- SAIR
+void sair(int &i)
+{ i++; }
+
